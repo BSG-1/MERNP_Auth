620d1e74890efb52818bb5d0b6de949e4815cd4e
Ghataorhe <b.ghataorhe@gmail.com>
2018-04-23 20:18:45 -0700

    last debug

--- Routes/users.js
+++ Routes/users.js
@@ -5,7 +5,7 @@ const { validateBody, schemas } = require('../Helpers/routeHelpers');
 const UsersController = require('../Controllers/users.js')
 
 router.route('/signup')
-    .post(validateBody(scemas.authSchema), UsersController.signUp);
+    .post(validateBody(schemas.authSchema), UsersController.signUp);
 
 router.route('/signin')
     .post(UsersController.signIn);
