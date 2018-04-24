9e9b78e12989be82b8d80706050aaecee6e7e58e
Ghataorhe <b.ghataorhe@gmail.com>
2018-04-23 20:17:26 -0700

    error fixes

--- Helpers/routeHelpers.js
+++ Helpers/routeHelpers.js
@@ -3,27 +3,27 @@
 const Joi = require('joi');
 
 module.exports = {
-    validateBody: (schema) = > {
+    validateBody: (schema) => {
         //receives one schema at a time
-        return(req, res, next) => {
-    //check whether data got passed by validation
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-        return res.status(400).json(result.error);
-    }
+        return (req, res, next) => {
+            //check whether data got passed by validation
+            const result = Joi.validate(req.body, schema);
+            if (result.error) {
+                return res.status(400).json(result.error);
+            }
 
-    //check if req.value is there, and if it isnt, set it to an empty object
-    if (!req.value) { req.value = (); }
-    req.value['body'] = result.value;
-    next()
+            //check if req.value is there, and if it isnt, set it to an empty object
+            if (!req.value) { req.value = {}; }
+            req.value['body'] = result.value;
+            next()
 
-}
+        }
     },
 
-schemas: {
-    authSchema: Joi.object().keys({
-        email: Joi.string().email().required();
-        password: Joi.string().required();
-    })
-}
+    schemas: {
+        authSchema: Joi.object().keys({
+            email: Joi.string().email().required(),
+            password: Joi.string().required()
+        });
+    }
 }
